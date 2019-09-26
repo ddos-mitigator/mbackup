57157852befd0a5e6133f2c3b727cd45494acc8c
Ilya Baev <baev@bifit.com>
2019-09-26 17:00:30 +0300

    рефакторинг + поддержка v19.08

--- __backup.py
+++ __backup.py
@@ -4,23 +4,15 @@ import copy
 import json
 import logging
 
-import mbase
-import mitigator
+import _mbase
+from __mrequest import M404Exception
 
 
 class Backup:
-    def __init__(self, _mitigator):
-        self.mitigator = _mitigator
-        self.config = mbase.Base(_mitigator)
-
-        self.policies = dict()
-        self.protection_params = dict()
-        self.rules = {'patches': list()}
-        self.groups = dict()
-        self.autodetect_params = dict()
-        self.bgp = dict()
-
     def create_backup(self, params=None):
+        if self._supported_version not in self.version:
+            sys.exit(f'FATAL ERROR: do not support backuping from {self.version}')
+
         logging.info('getting policies')
         self._get_policies()
         logging.info('getting protection parameters start')
@@ -34,10 +26,10 @@ class Backup:
         logging.info('getting bgp parameters')
         self._get_bgp_params()
 
-    def get_backup_as_json(self):
+    def get_backup_as_json(self, pretty=False):
         return json.dumps(
             {
-                'version': self.mitigator.version,
+                'version': self.version,
                 'data': {
                     'policies': self.policies,
                     'protection_params': self.protection_params,
@@ -48,11 +40,12 @@ class Backup:
                 },
             },
             ensure_ascii=False,
+            indent=4 if pretty else None,
         )
 
     # TODO: policies switches
     def _get_policies(self):
-        _policies = self.mitigator.make_request(uri='/policies/policies')['policies']
+        _policies = self.req(uri='/policies/policies')['policies']
         _policies.sort(key=lambda x: x['id'])
 
         for policy in _policies:
@@ -64,11 +57,8 @@ class Backup:
             self.policies[policy_id] = policy
 
             # policy switch
-            self.policies[policy_id].update(
-                self.mitigator.make_request(uri='/toggle/switch', policy=policy_id)
-            )
+            self.policies[policy_id].update(self.req(uri='/toggle/switch', policy=policy_id))
 
-    # TODO: autodetect
     def _get_protection_params(self):
         for _target in ['general', *self.policies]:
             self.protection_params[_target] = dict()
@@ -80,35 +70,37 @@ class Backup:
             else:
                 logging.info(f'getting protection parameters for policy with id {policy}')
 
-            for _cm_key, _cm_data in self.config.countermeasures.items():
+            for _cm_key, _cm_data in _mbase.countermeasures.items():
                 if _cm_data[key]:
                     self.protection_params[_target][_cm_key] = dict()
 
-                    # old _get_policy_prot_switches
-                    self.protection_params[_target][_cm_key]['switch'] = copy.deepcopy(_cm_data['switch'])
-                    self.config._get_countermeasure_switch(
-                        self.protection_params[_target][_cm_key]['switch'], policy
-                    )
+                    if 'switch' in _cm_data:
+                        self.protection_params[_target][_cm_key]['switch'] = copy.deepcopy(_cm_data['switch'])
+                        _mbase._get_countermeasure_switch(
+                            req_func=self.req,
+                            settings=self.protection_params[_target][_cm_key]['switch'],
+                            policy=policy,
+                        )
 
-                    # old _get_policy_prot_params
                     self.protection_params[_target][_cm_key]['settings'] = copy.deepcopy(_cm_data['settings'])
                     for _s_key, _s_data in _cm_data['settings'].items():
-                        _backup_func = _s_data.get('backup_func', self.config._get_simple)
+                        _backup_func = _s_data.get('backup_func', _mbase._get_simple)
                         try:
                             _backup_func(
+                                req_func=self.req,
                                 settings=self.protection_params[_target][_cm_key]['settings'][_s_key],
                                 policy=policy,
                             )
-                        except mitigator.M404Exception as e:
+                        except M404Exception as e:
                             logging.warning(e)
 
                         if 'backup_func' in _s_data:
                             del self.protection_params[_target][_cm_key]['settings'][_s_key]['backup_func']
 
-        mbase._recursive_cleanup(self.protection_params)
+        _mbase._recursive_cleanup(self.protection_params)
 
     def _get_groups(self):
-        _groups = self.mitigator.make_request(uri='/groups/groups')['groups']
+        _groups = self.req(uri='/groups/groups')['groups']
 
         for _group in _groups:
             _policies_list = list()
@@ -122,22 +114,22 @@ class Backup:
             del _group['rules']
             self.groups[_group_id] = copy.deepcopy(_group)
 
-        mbase._recursive_cleanup(self.groups)
+        _mbase._recursive_cleanup(self.groups)
 
     def _get_rules(self):
-        rules = self.mitigator.make_request(uri='/policySwitch/rules')['rules']
-        for rule in rules:
+        _rules = self.req(uri='/policySwitch/rules')['rules']
+        for rule in _rules:
             if 'is_default' in rule:
                 continue
 
             for index, patch in enumerate(self.rules['patches']):
-                _its_same_type_id = rule.get('type_id') == patch.get('type_id')
+                _type_id = rule.get('type_id')
+                _its_same_type_id = _type_id == patch.get('type_id')
                 _its_same_group_id = rule.get('group_id') == patch.get('group_id')
                 if _its_same_type_id and _its_same_group_id:
                     _patch = {
                         'op': 'add',
                         'value': {
-                            'version': 0,
                             'dst_prefix': rule.get('dst_prefix'),
                             'dst_port': rule.get('dst_port'),
                             'src_prefix': rule.get('src_prefix'),
@@ -146,14 +138,16 @@ class Backup:
                             'policy_id': rule.get('policy_id'),
                         },
                     }
-                    mbase._recursive_cleanup(_patch)
+                    if _type_id == 3:
+                        _patch['before'] = 1
+                    _mbase._recursive_cleanup(_patch)
                     self.rules['patches'][index]['patch'].append(_patch)
                     break
             else:
+                _type_id = rule.get('type_id')
                 _patch = {
                     'op': 'add',
                     'value': {
-                        'version': 0,
                         'dst_prefix': rule.get('dst_prefix'),
                         'dst_port': rule.get('dst_port'),
                         'src_prefix': rule.get('src_prefix'),
@@ -162,25 +156,42 @@ class Backup:
                         'policy_id': rule.get('policy_id'),
                     },
                 }
-                mbase._recursive_cleanup(_patch)
-                obj = {'type_id': rule.get('type_id'), 'group_id': rule.get('group_id'), 'patch': [_patch]}
+                if _type_id == 3:
+                    _patch['before'] = 1
+                _mbase._recursive_cleanup(_patch)
+                obj = {
+                    'type_id': _type_id,
+                    'group_id': rule.get('group_id'),
+                    'version': 1 if _type_id == 3 else 0,
+                    'patch': [_patch],
+                }
                 self.rules['patches'].append(obj)
 
     def _get_autodetect_params(self):
-        self.autodetect_params['switch'] = self.mitigator.make_request(uri='/autodetect/switch')
+        self.autodetect_params['switch'] = self.req(uri='/autodetect/switch')
         for policy in self.policies:
             self.autodetect_params[policy] = {'path': '/autodetect'}
-            self.config._get_autodetect_setting(settings=self.autodetect_params[policy], policy=policy)
+            _mbase._get_autodetect_setting(
+                req_func=self.req, settings=self.autodetect_params[policy], policy=policy
+            )
 
-        mbase._recursive_cleanup(self.autodetect_params)
+        _mbase._recursive_cleanup(self.autodetect_params)
 
     def _get_bgp_params(self):
         self.bgp['path'] = '/bgp'
-        self.bgp = self.mitigator.make_request(uri='/bgp')
+        self.bgp = self.req(uri='/bgp')
 
         self.bgp['neighbors_policies'] = dict()
         for neighbor in self.bgp.get('neighbors', list()):
             neighbor_id = neighbor['id']
-            self.bgp['neighbors_policies'][neighbor_id] = self.mitigator.make_request(uri=f'/bgp/neighbors/{neighbor_id}/policy')
+            self.bgp['neighbors_policies'][neighbor_id] = self.req(uri=f'/bgp/neighbors/{neighbor_id}/policy')
 
-        mbase._recursive_cleanup(self.bgp)
+        for index, prefix in enumerate(self.bgp.get('prefix_lists', list())):
+            if prefix.get('name') == 'system.flow.detect':
+                break
+        try:
+            del self.bgp['prefix_lists'][index]
+        except (KeyError, NameError):
+            pass
+
+        _mbase._recursive_cleanup(self.bgp)
--- mbackup.py
+++ mbackup.py
@@ -4,21 +4,22 @@ import argparse
 import sys
 import logging
 
+import mitigator
 
-PROG_VERSION = '2.1905.0'
+
+PROG_VERSION = '3.1908.4'
 
 
 def parse_options(args):
     parser = argparse.ArgumentParser()
 
-    # parser.add_argument('task', metavar='TASK', choices=['backup', 'restore', 'update-backup'])
-    parser.add_argument('task', metavar='TASK', choices=['backup', 'restore'])
+    parser.add_argument('task', metavar='TASK', choices=['backup', 'restore', 'update-file'])
     parser.add_argument('-v', '--version', action='version', version=PROG_VERSION)
     parser.add_argument('-s', '--server', metavar='URL', help='target server URL')
     parser.add_argument('-u', '--user', metavar='USERNAME', help='system admin username')
     parser.add_argument('-p', '--passwd', metavar='PASSWORD', help='system admin password')
     parser.add_argument('-q', '--quiet', action='store_true', help='disable logging')
-    # parser.add_argument('-ns', '--no_switch', action='store_true', help='if set, no restore switch values')
+    parser.add_argument('-pj', '--pretty-json', action='store_true', help='output json is pretty')
     parser.add_argument('-k', '--insecure', action='store_true', help='if set, no check SSL certificate')
     parser.add_argument(
         '-i',
@@ -34,59 +35,116 @@ def parse_options(args):
         type=argparse.FileType('x'),
         help='source file with json-format backup for restore',
     )
+    parser.add_argument(
+        '-c', '--config', metavar='PATH', type=argparse.FileType('r'), help='configuration file'
+    )
 
     return parser.parse_args(args)
+
+
+def main(options):
+    if options.config:
+        import configparser
+
+        _config = configparser.ConfigParser()
+        _config.read_file(options.config)
+
+        _section = options.task.upper()
+
+        if _section in _config:
+            _section_config = _config[_section]
+
+            _server = _section_config.get('server') or options.server
+            _user = _section_config.get('user') or options.user
+            _passwd = _section_config.get('password') or options.passwd
+            _output = (
+                open(_section_config.get('backup_target'), 'w')
+                if _section_config.get('backup_target')
+                else options.output
+            )
+            _input = (
+                open(_section_config.get('backup_source'), 'r')
+                if _section_config.get('backup_source')
+                else options.input
+            )
+            _insecure = _section_config.getboolean('insecure') or options.insecure
+            _pretty = _section_config.getboolean('pretty') or options.pretty_json
+
+        else:
+            sys.exit(f'section {_section} not in config file')
+
+    else:
+        _server = options.server
+        _user = options.user
+        _passwd = options.passwd
+        _output = options.output
+        _input = options.input
+        _insecure = options.insecure
+        _pretty = options.pretty_json
+
+    if _server and _user:
+        logging.info(f'execute params: server: {_server}; user: {_user}; insecure: {_insecure}')
+
+    if options.task == 'backup':
+        if not (_server and _user and _passwd and _output):
+            sys.exit(f'for backup --server, --user, --passwd and --output is required')
+
+        logging.info('getting authtoken on mitigator server')
+        _mitigator = mitigator.Mitigator(_server, _user, _passwd, _insecure)
+
+        logging.info('backuping start')
+        try:
+            _mitigator.create_backup()
+        except mitigator.MOthException as e:
+            logging.error(e)
+
+        logging.info(f'writing backup data in {_output.name}')
+        _output.write(_mitigator.get_backup_as_json(_pretty))
+
+    elif options.task == 'restore':
+        if not (_server and _user and _passwd and _input):
+            sys.exit(f'for restore --server, --user, --passwd and --input is required')
+
+        _mitigator = mitigator.Mitigator(_server, _user, _passwd, _insecure)
+
+        _file_data = _input.read()
+        if _file_data:
+            _mitigator.load_params_from_json(_file_data)
+        else:
+            sys.exit('source file is empty')
+
+        try:
+            _mitigator.restore()
+        except mitigator.MOthException as e:
+            logging.error(e)
+            logging.error('retry restore not support, reset your mitigator database')
+
+    elif options.task == 'update-file':
+        import __update
+
+        if not (_input and _output):
+            sys.exit(f'for update-file --input and --output is required')
+
+        _upd = __update.Update()
+
+        _file_data = _input.read()
+        if _file_data:
+            _upd.load_params_from_json(_file_data)
+        else:
+            sys.exit('source file is empty')
+
+        _upd.update_params()
+
+        logging.info(f'writing updated data in {_output.name}')
+        _output.write(_upd.get_params_as_json(_pretty))
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format='%(asctime)s: %(levelname)s: %(message)s')
 
-    # TODO: params from config file
-    options = parse_options(sys.argv[1:])
+    opt = parse_options(sys.argv[1:])
 
-    if options.task == 'backup':
-        if not (options.server and options.user and options.passwd and options.output):
-            sys.exit(f'for backup --server, --user, --passwd and --output is required')
-
-        import mitigator
-        import backup
-
-        logging.info('getting authtoken on mitigator server')
-        _mitigator = mitigator.Mitigator(options.server, options.user, options.passwd, options.insecure)
-        _backup = backup.Backup(_mitigator)
-
-        logging.info('backuping start')
-        try:
-            _backup.create_backup()
-        except mitigator.MOthException as e:
-            logging.error(e)
-
-        logging.info(f'writing backup data in {options.output.name}')
-        options.output.write(_backup.get_backup_as_json())
-
-    elif options.task == 'restore':
-        if not (options.server and options.user and options.passwd and options.input):
-            sys.exit(f'for restore --server, --user, --passwd and --input is required')
-
-        import mitigator
-        import restore
-
-        _mitigator = mitigator.Mitigator(options.server, options.user, options.passwd, options.insecure)
-        _restore = restore.Restore(_mitigator)
-
-        _file_data = options.input.read()
-        if _file_data:
-            _restore.load_params_from_json(_file_data)
-        else:
-            sys.exit('source file is empty')
-
-        try:
-            _restore.restore()
-        except mitigator.MOthException as e:
-            logging.error(e)
-            logging.error('retry restore not support, reset your mitigator database')
-
-    # elif options.task == 'file-update':
-    #     import file_update
-
-        # file_update.update()
+    try:
+        main(options=opt)
+    except mitigator.MOthException as e:
+        sys.exit(f'FATAL ERROR: {e}')
